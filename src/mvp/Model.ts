85218fe0275b32d6604d5475dba6555b061ae497
evgeny1993r <44282882+evgeny1993r@users.noreply.github.com>
2020-12-28 07:20:10 +0300

    Error edits

--- src/mvp/Model.ts
+++ src/mvp/Model.ts
@@ -22,10 +22,12 @@ class Model {
     this.minValue = minValue;
     this.maxValue = maxValue;
     if (currentValue.length === 1) {
-      this.currentValue[0] = Math.round(currentValue[0] / this.step) * this.step;
+      this.currentValue = [Math.round(currentValue[0] / this.step) * this.step];
     } else if (currentValue.length === 2) {
-      this.currentValue[0] = Math.round(currentValue[0] / this.step) * this.step;
-      this.currentValue[1] = Math.round(currentValue[1] / this.step) * this.step;
+      this.currentValue = [
+        Math.round(currentValue[0] / this.step) * this.step,
+        Math.round(currentValue[1] / this.step) * this.step,
+      ]
     }
     this.step = step;
     this.isShowValueWindow = isShowValueWindow;
