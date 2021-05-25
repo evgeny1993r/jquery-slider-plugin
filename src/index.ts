badb98ebe0342f74b14148860d7c0af1d36bc7b1
evgeny1993r <44282882+evgeny1993r@users.noreply.github.com>
2021-05-25 11:37:27 +0300

    fix errors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import './types/JQuery';
 
 import { Presenter } from './mvp/Presenter';
-import { IPresenter } from './types/mvp/IPresenter';
 
 (function ($) {
-  let presenter: IPresenter;
-
   $.fn.slider = function (key, value) {
+    this.presenter;
     this.init = ($this: JQuery, options?: IOptions) => {
       const settings = $.extend({
         $this,
@@ -38,40 +36,40 @@ import { IPresenter } from './types/mvp/IPresenter';
 
     this.setCurrentValue = (val: number[]) => {
       if (val.length === 1) {
-        presenter.setCurrentValue(val[0]);
+        this.presenter.setCurrentValue(val[0]);
       } else if (val.length === 2) {
         if (val[0] < val[1]) {
-          presenter.setCurrentValueMin(val[0]);
-          presenter.setCurrentValueMax(val[1]);
+          this.presenter.setCurrentValueMin(val[0]);
+          this.presenter.setCurrentValueMax(val[1]);
         } else if (val[0] > val[1]) {
-          presenter.setCurrentValueMin(val[1]);
-          presenter.setCurrentValueMax(val[0]);
+          this.presenter.setCurrentValueMin(val[1]);
+          this.presenter.setCurrentValueMax(val[0]);
         }
       }
     };
 
     this.setOrientation = (val: 'horizontal' | 'vertical') => {
-      presenter.setOrientation(val);
+      this.presenter.setOrientation(val);
     };
 
     this.setMinValue = (val: number) => {
-      presenter.setMinValue(val);
+      this.presenter.setMinValue(val);
     };
 
     this.setMaxValue = (val: number) => {
-      presenter.setMaxValue(val);
+      this.presenter.setMaxValue(val);
     };
 
     this.setStep = (val: number) => {
-      presenter.setStep(val);
+      this.presenter.setStep(val);
     };
 
     this.setIsShowValueWindow = (val: boolean) => {
-      presenter.setIsShowValueWindow(val);
+      this.presenter.setIsShowValueWindow(val);
     };
 
     this.setIsShowScaleValues = (val: boolean) => {
-      presenter.setIsShowScaleValues(val);
+      this.presenter.setIsShowScaleValues(val);
     };
 
     switch (key) {
@@ -98,9 +96,9 @@ import { IPresenter } from './types/mvp/IPresenter';
         break;
       default:
         if (!key) {
-          presenter = this.init(this);
+          this.presenter = this.init(this);
         } else if (typeof (key) === 'object') {
-          presenter = this.init(this, key);
+          this.presenter = this.init(this, key);
         }
     }
 
